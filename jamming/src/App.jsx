df8448958dfe6cedb6d6b904dafa830aee7db0c2
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-05-22 09:38:31 +0100

    install package

--- jamming/src/App.jsx
+++ jamming/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
+  const notify = () => toast("Merhaba! Bu bir toast mesajıdır.");
   return (
     <>
       <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button onClick={notify}>Toast Göster</button>
+      <ToastContainer />
     </>
   );
 }
