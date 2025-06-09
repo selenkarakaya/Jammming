7536d62d29379497ae4ca286987aaee23523ba28
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-06-09 13:47:51 +0100

    update h1 in search results

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -4,7 +4,7 @@ import Track from "./Track";
 function SearchResults({ tracks, addTrackToPlaylist }) {
   return (
     <div className="bg-white/15 p-4 rounded-xl shadow-md">
-      <h1 className="text-center">RESULTS</h1>
+      <h1 className="text-center text-lg font-semibold mb-2">Results</h1>
       {tracks.map((track, index) => (
         <Track
           track={track}
