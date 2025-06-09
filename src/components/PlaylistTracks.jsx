7ff0ef32bbe0171824e6c8cb7f2b4e96c16c8bc5
Selen Karakaya <122388950+selenkarakaya@users.noreply.github.com>
2025-06-09 23:14:20 +0100

    create playlist's tracks component

---
+++ src/components/PlaylistTracks.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+function PlaylistTracks({ track }) {
+  return (
+    <li key={track.id} style={{ marginBottom: "15px" }}>
+      <strong>{track.name}</strong> by {track.artist}
+      <br />
+      <em>Album:</em> {track.album}
+      <br />
+      {track.preview_url && (
+        <audio controls src={track.preview_url}>
+          Your browser does not support the audio element.
+        </audio>
+      )}
+      <br />
+      <a
+        href={track.external_urls.spotify}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Open in Spotify
+      </a>
+      <hr />
+    </li>
+  );
+}
+
+export default PlaylistTracks;
